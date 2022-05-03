c244bbfea3b3451bcb19ad66b5bbeb7b9149329a
Matt Teran <matthew.a.teran@gmail.com>
2022-05-03 15:34:53 -0500

    adds minimal sign up form + passport dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import moment from "moment";
 import Stopwatch from "./Stopwatch";
 import "./App.css";
+import Signup from "./Signup";
 
 function App() {
   const [time, setTime] = useState(moment().startOf("date"));
@@ -29,6 +30,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Signup />
         <Stopwatch time={time} />
         <button type="button" onClick={toggleTimer}>
           Study
---
+++ client/src/Signup.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Signup() {
+  return (
+    <div>
+      <input placeholder="Username" />
+      <input placeholder="Password" />
+      <button type="button">sign up</button>
+    </div>
+  );
+}
