7340e850cda9432310cc1e9e3c47dd456655d992
Matt Teran <matthew.a.teran@gmail.com>
2022-05-03 15:35:18 -0500

    adds some passport logic + db connection

--- server/index.js
+++ server/index.js
@@ -1,13 +1,38 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
-
-require("dotenv").config();
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var { User } = require("../database/index");
 
 const app = express();
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.post("/login", function (req, res) {
+  Users = new User({ email: req.body.email, username: req.body.username });
+
+  User.register(Users, req.body.password, function (err, user) {
+    if (err) {
+      res.json({
+        success: false,
+        message: "Your account could not be saved. Error: ",
+        err,
+      });
+    } else {
+      res.json({ success: true, message: "Your account has been saved" });
+    }
+  });
+});
 
 app.get("/", function (req, res) {
   return res.sendFile(path.join(__dirname, "../client/dist/index.html"));
