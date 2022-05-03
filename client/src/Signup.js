b2042b2dd3f71e9b7956a8f570b1883027083c7e
Matt Teran <matthew.a.teran@gmail.com>
2022-05-03 16:49:27 -0500

    connects sign up form to route

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,10 +1,60 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 export default function Signup() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const changeHandler = (ev) => {
+    if (ev.target.id === "username") {
+      setUsername(ev.target.value);
+    } else if (ev.target.id === "email") {
+      setEmail(ev.target.value);
+    } else if (ev.target.id === "password") {
+      setPassword(ev.target.value);
+    }
+  };
+
+  const submitHandler = () => {
+    // validate inputs
+
+    axios
+      .post("/api/signup", {
+        username,
+        email,
+        password,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div>
-      <input placeholder="Username" />
-      <input placeholder="Password" />
-      <button type="button">sign up</button>
+      <input
+        placeholder="Email Address"
+        id="email"
+        onChange={changeHandler}
+        value={email}
+      />
+      <input
+        placeholder="Username"
+        id="username"
+        value={username}
+        onChange={changeHandler}
+      />
+      <input
+        placeholder="Password"
+        id="password"
+        onChange={changeHandler}
+        value={password}
+      />
+      <button type="button" onClick={submitHandler}>
+        sign up
+      </button>
     </div>
   );
 }
